706d5b3dd2ce0eb33d50e98a641196115268b984
Mikka Akdag <mia011@edu.zealand.dk>
2025-11-26 13:33:13 +0100

    erstattet kode med et jeg har lavet

--- js/main.js
+++ js/main.js
@@ -2,12 +2,30 @@ const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            Word: null,
+            Words: [],
+            Message: null
         }
     },
     methods: {
         myMethod(){
 
         },
+        save(Word) {
+            this.Words.push(Word)
+        },
+        clear() {
+            this.Words = []
+            this.Message = null
+        },
+        show() {
+            if (this.Words == null || this.Words.length == 0) {
+                this.Message = "empty"
+            }
+            else {
+                this.Message = this.Words.toString()
+            }
+        }
     },
     computed: {
         myComputed() {
